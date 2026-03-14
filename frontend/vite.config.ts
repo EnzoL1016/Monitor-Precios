2275af9409b3443da94882dcd2861f913d8e951e
Enzo Lopez <enzolopez1016@gmail.com>
2026-03-14 20:21:38 -0300

    produccion

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,8 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   server: {
     port: 5173,
-    host: true // Permite acceso externo si fuera necesario
+    host: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      }
+    }
   }
 })
