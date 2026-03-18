d9c5351cad80350f69f57e3c849ed338ad48d694
Enzo Lopez <enzolopez1016@gmail.com>
2026-03-18 03:22:31 -0300

    reemplazando celery

--- backend/core/celery.py
+++ backend/core/celery.py
@@ -1,11 +1,1 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings') # <--- CLAVE
-
-app = Celery('scraper_project')
-
-# El namespace='CELERY' obliga a que en settings.py las variables empiecen con CELERY_
-app.config_from_object('django.conf:settings', namespace='CELERY')
-
-app.autodiscover_tasks()
+# Celery removed - using APScheduler instead
